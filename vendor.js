18ddd3dd55a22a92846b0a24616219f12af80f64
Eduardo Garcia <egarcia@ooyala.com>
2015-08-22 14:41:45 -0500

    Move FF patch for focus* to vendor

--- vendor.js
+++ vendor.js
@@ -67,6 +67,40 @@ if (!Array.prototype.indexOf) {
   };
 }
 
+// Firefox patch for focusin, focusout events
+// https://gist.github.com/nuxodin/9250e56a3ce6c0446efa
+(function() {
+  var w = window,
+      d = w.document;
+
+  function addPolyfill(e) {
+    var type = e.type === 'focus' ? 'focusin' : 'focusout';
+    var event = new CustomEvent(type, { bubbles: true, cancelable: false });
+    event.c1Generated = true;
+    e.target.dispatchEvent( event );
+  }
+
+  function removePolyfill(e) {
+    if(!e.c1Generated) { // focus after focusin, so chrome will the first time trigger tow times focusin
+        d.removeEventListener('focus'   , addPolyfill   , true);
+        d.removeEventListener('blur'    , addPolyfill   , true);
+        d.removeEventListener('focusin' , removePolyfill, true);
+        d.removeEventListener('focusout', removePolyfill, true);
+    }
+    setTimeout(function () {
+        d.removeEventListener('focusin' , removePolyfill, true);
+        d.removeEventListener('focusout', removePolyfill, true);
+    });
+  }
+
+  if(typeof w.onfocusin === 'undefined') {
+    d.addEventListener('focus'   , addPolyfill   , true);
+    d.addEventListener('blur'    , addPolyfill   , true);
+    d.addEventListener('focusin' , removePolyfill, true);
+    d.addEventListener('focusout', removePolyfill, true);
+  }
+})();
+
 var Util = {
   // https://github.com/jashkenas/underscore/blob/master/underscore.js#L800
   throttle: function (func, wait, options) {
