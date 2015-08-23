8cc1eb3f275eca8b8b986ea616c53095e1ffe833
Eduardo Garcia <egarcia@ooyala.com>
2015-08-22 22:58:23 -0500

    Add teardown to Watchmen, attach to window, log object instead of string

--- src/Watchmen.js
+++ src/Watchmen.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var Watchmen = {
+  // Init all dependencies
   init: function (events) {
     BehaviourLogger.init(events);
   },
 
+  // Teardown all dependencies
+  teardown: function () {
+    BehaviourLogger.teardown();
+  },
+
+  // Used in case events are cancelled/stopped for some reason.
   logCustomEvent: function (event) {
     event.isCustom = true;
-    Logger.log(JSON.stringify(event));
+    Logger.log(event);
   }
 };
+
+window.Watchmen = Watchmen;
